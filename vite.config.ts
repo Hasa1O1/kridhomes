56b1f9777932bed8a61712bfb9a46cc95b63612d
Hasa1O1 <your.email@domain.com>
2026-08-25 00:33:46 +0200

    before host test

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import { resolve } from 'node:path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  publicDir: 'public',
   build: {
+    outDir: 'dist',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
